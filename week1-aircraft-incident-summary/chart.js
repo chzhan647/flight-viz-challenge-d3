1a7866acc0df81f165db41e12783a6c3eba0563b
Yuxin Zhang <yuxinzhang@yuxins-mbp.mynetworksettings.com>
2024-04-28 22:47:58 -0400

    adding wrapper and bounds

--- week1-aircraft-incident-summary/chart.js
+++ week1-aircraft-incident-summary/chart.js
@@ -1,7 +1,9 @@
+//Reference to Newline.co D3 courses 
+async function drawLineChart() {
 // 1. Create chart parameters 
 let dimensions = {
     width: window.innerWidth * 0.9,
-    height: 400,
+    height: 200,
     margin: {
       top: 15,
       right: 15,
@@ -9,18 +11,23 @@ let dimensions = {
       left: 60,
     },
   }
-  dimensions.boundedWidth = dimensions.width
-    - dimensions.margin.left
-    - dimensions.margin.right
-  dimensions.boundedHeight = dimensions.height
-    - dimensions.margin.top
-    - dimensions.margin.bottom
-
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top  - dimensions.margin.bottom
 // 2. Access data and parse information 
-filename = "../data/year_counts.csv"
-const x = d => dataParser(d.UTC_YEAR)
-const y = d => d.count 
-const dateParser = d3.timeParse("%Y-%m-%d")
+
+  try {
+    const data = await d3.csv("../data/year_count.csv")
+    console.log(data)
+
+    const x = d => dataParser(d.UTC_YEAR)
+    const y = d => d.count 
+    const dateParser = d3.timeParse("%Y")
+
+  } catch (error) {
+    console.error("An error occurred while loading the CSV file:", error);
+  }
+
+
 
 //3. Set ranges 
 const yScale = d3.scaleLinear()
@@ -33,6 +40,25 @@ const xScale = d3.scaleTime()
 
 // 4. Create SVG canvas with HTML element
 const wrapper = d3.select("#wrapper")
-.append("svg")
-.attr("width", dimensions.width)
-.attr("height",dimensions.height)
+  .append("svg")
+    .attr("width", dimensions.width) //update the attribute of the element change the svg size 
+    .attr("height",dimensions.height)
+
+
+const bounds = wrapper.appeand("g") //group element for svg in a group 
+  .style("transform",`translate(${
+    dimensions.margins.left
+  }px, ${
+    dimensions,margins.top
+  }px)`)
+
+
+// Draw data elements 
+const line = bounds.append("path")
+  .attr("d")
+
+}
+
+drawLineChart()
+
+
